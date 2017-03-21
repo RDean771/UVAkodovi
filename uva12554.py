2a1cc3b19296fabb1257b114b8b8aff165bdb5b0
RDean771 <rdean.771@gmail.com>
2017-03-22 00:16:15 +0100

    A Special "Happy Birthday" Song!!!

---
+++ uva12554.py
@@ -0,0 +1,12 @@
+rijeci=["Happy","birthday","to","you",\
+        "Happy","birthday","to","you",\
+        "Happy","birthday","to","Rujia",\
+        "Happy","birthday","to","you"]
+imena=[]
+N=int(input())
+for i in range(0,N):
+    imena.append(input())
+brpon=((N//16)+1)*16
+for i in range(0,brpon):
+    print("{}: {}".format(imena[i%N],rijeci[i%16]))
+    
