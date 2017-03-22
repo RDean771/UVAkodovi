5d75525ab0f606441cf4914d788235d6a3211a43
RDean771 <rdean.771@gmail.com>
2017-03-22 02:09:37 +0100

    Zeros and Ones
    unfinished (infinite loop in test case)

---
+++ uva10324.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(void)
+{
+	string s;
+	int k=0;
+	while(getline(cin,s))
+	{
+		k++;
+		int n;
+		cin >> n;
+		cout << "Case " << k << ":" << endl;
+		for (int i=0;i<n;i++)
+		{
+			int ind1,ind2,manji,veci;
+			cin >> ind1 >> ind2;
+			manji=(ind1 < ind2)? ind1: ind2;
+			veci=ind1+ind2-manji;
+			bool flag=1;
+			if(veci-manji>=2)
+			{
+				for(int j=manji+1;j<=veci;j++)
+				{
+					if(s[j-1]!=s[j]) flag=0;
+				}
+			}
+			if(flag) cout << "Yes" << endl;
+			else cout << "No" << endl;
+		}
+	}
+	return 0;
+}
